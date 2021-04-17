80bc1702e49171798635c03dfb5ebdbdbf562efe
slava0135 <super.novalskiy_0135@inbox.ru>
2021-04-17 20:30:05 +0300

    compilation

--- compilation/main.c
+++ compilation/main.c
@@ -1,21 +1,31 @@
+#include "merge.h"
+
 #include <malloc.h>
 #include <stddef.h>
 #include <stdio.h>
-#include "merge.h"
 
-static int array1[] = {2, 5, 6, 8};
-static int array2[] = {0, 1, 3, 4, 7, 9, 10};
-
-static size_t length1 = sizeof array1 / sizeof array1[0];
-static size_t length2 = sizeof array2 / sizeof array2[0];
+static void printArray(const int* array, const size_t length)
+{
+    for (int i = 0; i < length; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
 
 int main()
 {
+    const int array1[] = {2, 5, 6, 8};
+    const int array2[] = {0, 1, 3, 4, 7, 9, 10};
+
+    const size_t length1 = sizeof array1 / sizeof array1[0];
+    const size_t length2 = sizeof array2 / sizeof array2[0];
+
+    printArray(array1, length1);
+    printArray(array2, length2);
+
     int *merged = merge(array1, length1, array2, length2);
-    for (int i = 0; i < length1 + length2; i++) {
-        printf("%d ", merged[i]);
-    }
+    printArray(merged, length1 + length2);
+
     free(merged);
     return 0;
 }
-
--- compilation/merge.h
+++ compilation/merge.h
@@ -1,7 +1,7 @@
-#include <stddef.h>
-
 #ifndef MERGE_H
 #define MERGE_H
+
+#include <stddef.h>
 
 extern int* merge(const int array1[], size_t length1, const int array2[], size_t length2);
 
